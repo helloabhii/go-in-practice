2eea36a3817f51ae60fc085af26d639d84de4033
Abhishek Verma <ak8888780@gmail.com>
2024-01-31 23:23:38 +0530

    mocktesting

---
+++ ch5/testing/testingMock/mockTestin.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func main() {
+}
+
+type Messager interface {
+	Send(email, subject string, body []byte) error
+}
+
+func Alert(m Messager, problem []byte) error {
+	return m.Send("noc@example.com", "this is subject", problem)
+}
+
+type MockMessage struct {
+	email, subject string
+	body           []byte
+}
+
+func (m *MockMessage) Send(email, subject string, body []byte) error {
+	m.email = email
+	m.subject = subject
+	m.body = body
+	return nil
+}
+
+func TestAlert(t *testing.T) {
+	msgr := new(MockMessage)
+	body := []byte("Critical Error")
+
+	Alert(msgr, body)
+
+	if msgr.subject != "Critical Error" {
+		t.Errorf("Expected 'Critical Error', Got '%s'", msgr.subject)
+	}
+}
